7cf24c17cd82c03dee2f1c6e140527ec55ea5165
Girish Girijan <girishvazheliparambil@gmail.com>
2024-05-27 17:33:12 -0300

    Completed Logout functionality in UI

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,15 +1,32 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/authContext";
 
 const useLogout = () => {
+  const { setAuthUser } = useAuthContext();
   const [loading, setLoading] = useState(false);
-  setLoading(true);
-  try {
-  } catch (error) {
-    toast.error(error.message);
-  } finally {
-    setLoading(false);
-  }
+
+  const logout = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/logout", {
+        method: "POST",
+        headers: { "Context-Type": "application/json" },
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        toast.error(data.message);
+        return false;
+      }
+      localStorage.removeItem("chat-user");
+      setAuthUser(null);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { loading, logout };
 };
 
 export default useLogout;
