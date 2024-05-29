6584c5aacf3380c1a740ec5c1ef98537a1ac098e
Girish Girijan <girishvazheliparambil@gmail.com>
2024-05-28 23:32:35 -0300

    Implemented real time chat, showing online users and sound

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -2,6 +2,7 @@ import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import Conversation from "../models/conversation.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 //Creating a new message
 export const sendMessage = async (req, res, next) => {
@@ -35,6 +36,12 @@ export const sendMessage = async (req, res, next) => {
       await Promise.all([conversation.save(), newMessage.save()]);
 
       //Socket functionality
+      const receiverSocketId = getReceiverSocketId(receiverId);
+      if (receiverSocketId) {
+        // io.to(socket.id).emit() used to send events to specific client
+        io.to(receiverSocketId).emit("newMessage", newMessage);
+      }
+
       res.status(201).json(newMessage);
     } else {
       return next(errorHandler(500, "Internal server error"));
@@ -55,7 +62,7 @@ export const getMessages = async (req, res, next) => {
     }).populate("messages");
 
     if (!conversation) return res.status(200).json([]);
-    
+
     res.status(200).json(conversation.messages);
   } catch (error) {
     next(error);
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -2,9 +2,13 @@ import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../../components/skeletons/MessageSkeleton";
 import { useEffect, useRef } from "react";
+import useListenMessages from "../../hooks/useListenMessages";
+import useConversation from "../../zustand/useConversation";
 
 export default function Messages() {
+  useListenMessages();
   const { loading, messages } = useGetMessages();
+  const { selectedConversation } = useConversation();
   const lastMessageRef = useRef();
 
   useEffect(() => {
@@ -17,16 +21,23 @@ export default function Messages() {
     <div className="px-4 flex-1 overflow-auto">
       {loading &&
         [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
+
       {!loading && messages.length === 0 && (
         <p className="text-center">Send a message to start the conversation</p>
       )}
       {!loading &&
         messages.length > 0 &&
-        messages.map((message) => (
-          <div key={message._id} ref={lastMessageRef}>
-            <Message message={message} />
-          </div>
-        ))}
+        messages.map((message) => {
+          if (
+            message.senderId === selectedConversation._id ||
+            message.receiverId === selectedConversation._id
+          )
+            return (
+              <div key={message._id} ref={lastMessageRef}>
+                <Message message={message} />
+              </div>
+            );
+        })}
     </div>
   );
 }
