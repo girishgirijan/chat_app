7ae2347154835518480a2d549ab31a52ad9b63c3
Girish Girijan <girishvazheliparambil@gmail.com>
2024-05-28 21:11:18 -0300

    Implemented Online users functionality

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import { app, server } from "./socket/socket.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js"
-import userRoutes from "./routes/user.routes.js"
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-const app = express();
+//const app = express();
 
 //Middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -22,17 +23,16 @@ app.use("/api/user", userRoutes);
 
 //Middleware to handle error
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
-  
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${port}`);
 });
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,8 +1,11 @@
+import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../zustand/useConversation";
 
 export default function Conversation({ conversation, emoji, lastIndex }) {
   const { selectedConversation, setSelectedConversation } = useConversation();
   const isSelected = selectedConversation?._id === conversation._id;
+  const {onlineUsers} = useSocketContext();
+  const isOnline = onlineUsers?.includes(conversation._id)
   return (
     <>
       <div
@@ -11,7 +14,7 @@ export default function Conversation({ conversation, emoji, lastIndex }) {
         }`}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? "online" : ""} `}>
           <div className="w-12 rounded-full">
             <img src={conversation.profilePic} />
           </div>
