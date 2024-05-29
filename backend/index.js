8040758d6e5944e0b4a17744608a4737c47064e2
Girish Girijan <girishvazheliparambil@gmail.com>
2024-05-28 23:53:12 -0300

    Setup for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path"
 
 import { app, server } from "./socket/socket.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -11,6 +12,7 @@ import userRoutes from "./routes/user.routes.js";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 //const app = express();
 
 //Middlewares
@@ -30,6 +32,13 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+//For deployment
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(port, () => {
